46005d8ea529c2b286e80614d1dd0498949584b3
OpenMous <adrianmostazoruiz@gmail.com>
2026-08-12 17:33:18 +0200

    Versión del menú con opcion a añadir opciones dentro del script

--- prototipe.sh
+++ prototipe.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script prototipo para generar menus automaticamente by | AntonioOA1206 & mosto
 # Funciones
+##
 function plantilla_prompt ( # Función plantilla para colorear el "prompt"
 	echo -e "\e[${1}m${@:2}\e[0m"
 )
@@ -22,61 +23,103 @@ function print_menu ( # Función que colorea el menú
 	done
 )
 ##
-# Variables
-posicion_menu=1
-# Array de opciones
-declare -a opciones=("Ver_Usuarios" "Crear_Usuario" "Modificar_Usuario" "Borrar_Usuarios" "Volcar_y_Salir")
-# Menú interactivo
-while true; do
-	tput civis # Oculta el cursor en la terminal
-	##
+posicion_menu=1 # Posición inicial
+menu_interactivo() {
 	while true; do
-		# Array que guarda los argumentos (colores) y se reinicia cada que te mueves por el menú
-		declare -a argumentos=(30) # Argumento 30 = cadena de iguales (======)
-		# Contador que incluye al array argumentos tantos como opciones -1 haya
-		for ((i=1; i<${#opciones[@]}; i++)); do
-			argumentos+=(33) # Argumento 33 = Opción del menú
-		done
-		argumentos+=(31) # Argumento 31 = Opción de salir del menú (rojo)
-		argumentos[$posicion_menu]=44 # Argumento 44 = Opción seleccionada 
+		tput civis # Oculta el cursor en la terminal
 		##
-		print_menu "${argumentos[@]}"
-		read -s -n 1 tecla # Imput del teclado para mover el menú (WS)
-		##
-		if [ -z $tecla ]; then # Enter
-			break
-		##
-		elif [ $tecla = "w" ]; then # W
-			if [ $posicion_menu -eq 1 ];then
-				posicion_menu=1
+		while true; do
+			# Array que guarda los argumentos (colores) y se reinicia cada que te mueves por el menú
+			declare -a argumentos=(30) # Argumento 30 = cadena de iguales (======)
+			# Contador que incluye al array argumentos tantos como opciones -1 haya
+			for ((i=1; i<${#opciones[@]}; i++)); do
+				argumentos+=(33) # Argumento 33 = Opción del menú
+			done
+			argumentos+=(31) # Argumento 31 = Opción de salir del menú (rojo)
+			argumentos[$posicion_menu]=44 # Argumento 44 = Opción seleccionada 
+			##
+			print_menu "${argumentos[@]}"
+			read -s -n 1 tecla # Imput del teclado para mover el menú (WS)
+			##
+			if [ -z $tecla ]; then # Enter
+				break
+			##
+			elif [ $tecla = "w" ]; then # W
+				if [ $posicion_menu -eq 1 ]; then
+					posicion_menu=1
+				else
+					posicion_menu=$(($posicion_menu-1))
+				fi
+			##
+			elif [ $tecla = "s" ]; then # S
+				if [ $posicion_menu -eq ${#opciones[@]} ]; then
+					posicion_menu=${#opciones[@]}
+				else
+					posicion_menu=$((posicion_menu+1))
+				fi
+			##
 			else
-				posicion_menu=$(($posicion_menu-1))
+				posicion_menu=$posicion_menu
 			fi
-		##
-		elif [ $tecla = "s" ]; then # S
-			if [ $posicion_menu -eq ${#opciones[@]} ];then
-				posicion_menu=${#opciones[@]}
-			else
-				posicion_menu=$((posicion_menu+1))
-			fi
-		##
-		else
-			posicion_menu=$posicion_menu
-		fi
-		##
-		clear
-	done
-	tput cnorm
-	##
-	if [ $posicion_menu -eq 5 ]; then # Opción para salir
-		break
-	else
-		read -p "¿Deseas algo mas (S/n)? " emp
-		if [ -z $emp ] || [ $emp = "s" ] || [ $emp = "S" ];then
+			##
 			clear
+		done
+		tput cnorm
+		##
+		if [ $posicion_menu -eq ${#opciones[@]} ]; then # Opción para salir
+			break
 		else
+			read -p "¿Deseas algo mas (S/n)? " sn3
+			if [ -z $sn3 ] || [ $sn3 = "s" ] || [ $sn3 = "S" ]; then
+				clear
+			else
+				break
+			fi
+		fi
+		##
+	done
+}
+##
+# Variables
+fichero_opciones="./opciones.txt"
+opciones_introducidas=$(cat $fichero_opciones)
+# Programa
+clear # Clear del terminal
+##
+echo "Procesando opciones..."
+sleep 1
+echo "-- Opciones introducidas --"
+cat $fichero_opciones
+##
+echo "==========="
+echo "1. Crear menu con estas opciones"
+echo "2. Crear menu con nuevas opciones"
+echo "3. Salir"
+echo "==========="
+read -p "¿Que quieres hacer? " pre_menu
+##
+case $pre_menu in
+	1)
+	declare -a opciones
+	mapfile -t opciones < "$fichero_opciones"
+	menu_interactivo
+	;;
+	2)
+	read -p "Nueva opcion (ENTER para salir): " nueva_opcion
+	echo "$nueva_opcion" > $fichero_opciones
+	while true; do
+		read -p "Nueva opcion (ENTER para salir): " nueva_opcion
+		if [ "$nueva_opcion" == "" ]; then
 			break
 		fi
-	fi
-	##
-done
+		echo "$nueva_opcion" >> $fichero_opciones
+	done
+	declare -a opciones
+	mapfile -t opciones < "$fichero_opciones"
+	menu_interactivo
+	;;
+	*)
+	echo "Saliendo..."
+	;;
+esac
+##
